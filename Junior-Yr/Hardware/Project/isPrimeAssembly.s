f215e448994bbac66e6de189f03950eb784684d8
Aidan C <terminatedtank@gmail.com>
2024-11-04 13:00:37 -0500

    10-31-2024 eosX13

---
+++ Junior-Yr/Hardware/Project/isPrimeAssembly.s
@@ -0,0 +1,11 @@
+	.globl isPrimeAssembly
+
+isPrimeAssembly:
+	//your code for iterating through arrays here
+	//base addresses of arrays in X0 - X2, length in X3s
+nop	//first line of execution when called by main.c
+
+
+isPrime:
+	//Your code for detecting a prime number here
+	
